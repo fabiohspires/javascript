d79236e2a393ccccdc2c7f225d33a186defceb92
Fábio Pires <103321577+fabiohspires@users.noreply.github.com>
2022-05-25 17:58:44 -0300

    aula 1

---
+++ js/instring.js
@@ -0,0 +1,12 @@
+// String
+const message = 'Hello world!'
+console.log(message.length)
+
+const firstname = 'Fabio'
+const lastname = 'Pires'
+
+const names = 'Fabio,julia,João'
+
+console.log(`Meu nome é ${firstname.toUpperCase} ${lastname.toLowerCase}`);
+
+console.log(names.split(","))
---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+// Number
+const number = 5
+
+console.log(number + 10)
+
+
+// Booleans
+false
+true
+
+console.log(2 == 3)
+
+
+// Null & Undefined
+const x = null;
+
+const y = undefined;
