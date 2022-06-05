bb3500dd159894cdf6aed4ca2278968d284c7577
Fábio Pires <103321577+fabiohspires@users.noreply.github.com>
2022-06-05 20:56:34 -0300

    att

---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'eua'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log(`Você nao mora no Brasil e sim no ${pais}`)
+} else{
+    console.log(`Voce é brasileiro, que legal!`)
+}
