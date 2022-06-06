86da0f9fc6da6c3917193f8104747370227892e3
Fábio Pires <103321577+fabiohspires@users.noreply.github.com>
2022-06-06 17:02:24 -0300

    aaa

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,10 @@
+function carregar() {
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora <12 ){
+        
+    }
+}
